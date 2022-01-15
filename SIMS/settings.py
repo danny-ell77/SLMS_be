be395349089ba864f6ab33ce8f28bc0f0a9894f8
Daniel Olah <danielolahskybrow@gmail.com>
2022-01-15 13:11:13 +0100

    Automatic configuration with django-heroku

--- SIMS/settings.py
+++ SIMS/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
