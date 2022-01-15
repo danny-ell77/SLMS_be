ca17e01d734e703a60b59b298cc77a4193dcc114
Daniel Olah <danielolahskybrow@gmail.com>
2022-01-15 13:36:00 +0100

    resolved django-heroku 'BASE_DIR' error

--- SIMS/settings.py
+++ SIMS/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from datetime import timedelta
 
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -177,3 +176,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
